ceafb7dfe47a73a9f3164a3c317b434aa73296e5
mbrandler <marya.brandler@yandex.ru>
2020-05-29 21:39:05 +0300

    Initial commit

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './app.css'
+import TopSection from "../top-section";
+
+const App = () => {
+    return (
+        <div className="app">
+            <TopSection />
+        </div>
+    )
+};
+
+export default App;
---
+++ src/components/top-section/top-section.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './top-section.css'
+import Header from "../header";
+
+const TopSection = () => {
+    return (
+        <div className="top-section">
+            <Header />
+            <h1>Hi, It’s Théophile Seyrig</h1>
+        </div>
+    )
+};
+
+export default TopSection;
