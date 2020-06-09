ea4c445779b7cfa2a855cc6f3d342d01eadba038
mbrandler <marya.brandler@yandex.ru>
2020-06-09 16:05:16 +0300

    Updates

---
+++ precache-manifest.023569df97e672e832728c041cbdb4ca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c48ea1b9ca847bb8e857154d99126a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c68cc76e5d86569fc44a",
+    "url": "/Portfolio/static/css/main.35b44b87.chunk.css"
+  },
+  {
+    "revision": "9fb1989eab2682d8abef",
+    "url": "/Portfolio/static/js/2.abb04f5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.abb04f5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c68cc76e5d86569fc44a",
+    "url": "/Portfolio/static/js/main.ba33d55d.chunk.js"
+  },
+  {
+    "revision": "c66aa5eafc120060b1dc",
+    "url": "/Portfolio/static/js/runtime-main.efb1fe8d.js"
+  },
+  {
+    "revision": "5a461d538f2e4a1b724a1405330c9d37",
+    "url": "/Portfolio/static/media/arrow.5a461d53.svg"
+  },
+  {
+    "revision": "4e95fac7972022d794c42cee8b37fc7c",
+    "url": "/Portfolio/static/media/bg.4e95fac7.jpg"
+  },
+  {
+    "revision": "23ac348a0b56b9ba54ca1cc9614ec86b",
+    "url": "/Portfolio/static/media/image_1.23ac348a.jpg"
+  },
+  {
+    "revision": "203f468dcdff964dce6c7526284abac7",
+    "url": "/Portfolio/static/media/image_2.203f468d.jpg"
+  },
+  {
+    "revision": "99fec3c3fed80a44957709416be12936",
+    "url": "/Portfolio/static/media/image_3.99fec3c3.jpg"
+  },
+  {
+    "revision": "7bc3be56e6f25c3d0f74b55d32390826",
+    "url": "/Portfolio/static/media/image_4.7bc3be56.jpg"
+  },
+  {
+    "revision": "676bd965965503f154ed8bf1903f1aee",
+    "url": "/Portfolio/static/media/menu.676bd965.svg"
+  },
+  {
+    "revision": "743b8c642a409a764e38b01b3e5e39e9",
+    "url": "/Portfolio/static/media/profile-picture.743b8c64.jpg"
+  }
+]);
